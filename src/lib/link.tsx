ff852abd18a8549e4f0c1f3d6b49651096088d8b
M. Fazri Nizar <anon6372098.id@gmail.com>
2024-12-01 03:57:56 +0700

    Feat: add fonts, styles and structure of code

---
+++ src/lib/link.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const links = [
+    { id: 0, title: "Home", path: "/" },
+    { id: 1, title: "Menu", path: "/menu" },
+    { id: 2, title: "About", path: "/about" },
+    { id: 3, title: "Contact", path: "/contact" },
+  ];
