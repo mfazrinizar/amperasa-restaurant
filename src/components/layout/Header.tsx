6d82fe301e042bafd20b349febde3e2f87d16af6
M. Fazri Nizar <anon6372098.id@gmail.com>
2024-12-02 01:46:44 +0700

    Fix: Header Menu Always Hovering on Dashboard

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,104 +9,104 @@ import Hamburger from "@/components/ui/Hamburger";
 import useUserNav from "@/hooks/useUser";
 
 export default function Header() {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const pathname = usePathname();
-  const isHomepage = pathname === "/";
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const pathname = usePathname();
+    const isHomepage = pathname === "/";
 
-  const user = useUserNav();
+    const user = useUserNav();
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50);
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50);
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    useEffect(() => {
+        // Close the menu when navigating to a new route
+        setIsMenuOpen(false);
+    }, [pathname]);
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen((prev) => !prev);
     };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
 
-  useEffect(() => {
-    // Close the menu when navigating to a new route
-    setIsMenuOpen(false);
-  }, [pathname]);
+    const handleMenuItemClick = () => {
+        setTimeout(() => {
+            setIsMenuOpen(false);
+        }, 1000);
+    };
 
-  const handleMenuToggle = () => {
-    setIsMenuOpen((prev) => !prev);
-  };
-
-  const handleMenuItemClick = () => {
-    setTimeout(() => {
-      setIsMenuOpen(false);
-    }, 1000);
-  };
-
-  return (
-    <>
-      {/* Backdrop for shadowing with animation */}
-      <div
-        className={clsx(
-          "fixed top-0 left-0 z-30 w-full h-full transition-all duration-300 bg-black",
-          isMenuOpen ? "opacity-50 visible" : "opacity-0 invisible"
-        )}
-        onClick={handleMenuToggle}
-      ></div>
-
-      <nav
-        id="Header"
-        className={clsx(
-          "z-40 w-full transition",
-          isHomepage &&
-            (isScrolled || isMenuOpen ? "bg-neutral-900" : "bg-neutral-900/60"),
-          isHomepage ? "fixed top-0" : "sticky top-0 bg-neutral-900"
-        )}
-      >
-        <div className="container">
-          <div className="w-full py-4 mx-auto lg:flex md:justify-between md:items-center">
-            {/* Logo and Hamburger */}
-            <div className="flex items-center justify-between w-full">
-              <Link
-                href="/"
-                className="flex items-center justify-center gap-2 text-2xl font-semibold text-neutral-100"
-              >
-                <LogoSVG width={50} height={50} bg={false} />
-                <span className="flex">
-                  <span className="text-primary-500 font-brush">Ampe</span>
-                  <span className="text-white font-brush">RASA</span>
-                </span>
-              </Link>
-              <Hamburger isOpen={isMenuOpen} setIsOpen={handleMenuToggle} />
-            </div>
-
-            {/* Navigation Links */}
+    return (
+        <>
+            {/* Backdrop for shadowing with animation */}
             <div
-              className={clsx(
-                isMenuOpen ? "opacity-100 visible" : "opacity-0 invisible",
-                "lg:visible transition absolute inset-x-0 z-40 w-full px-6 py-4 duration-300 ease-in-out bg-neutral-900 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center"
-              )}
+                className={clsx(
+                    "fixed top-0 left-0 z-30 w-full h-full transition-all duration-300 bg-black",
+                    isMenuOpen ? "opacity-50 visible" : "opacity-0 invisible"
+                )}
+                onClick={handleMenuToggle}
+            ></div>
+
+            <nav
+                id="Header"
+                className={clsx(
+                    "z-40 w-full transition",
+                    isHomepage &&
+                    (isScrolled || isMenuOpen ? "bg-neutral-900" : "bg-neutral-900/60"),
+                    isHomepage ? "fixed top-0" : "sticky top-0 bg-neutral-900"
+                )}
             >
-              <div className="flex flex-col items-center gap-8 lg:flex-row lg:mx-6">
-                {links.map(({ id, title, path }) => (
-                  <Link
-                    key={id}
-                    onClick={pathname == path || (pathname === "/dashboard" && path === "/login" ) ? handleMenuItemClick : handleMenuToggle}
-                    className={clsx(
-                      "relative my-2 transition-colors duration-300 transform md:mx-4 md:my-0",
-                      pathname == path
-                        ? "text-primary-400"
-                        : "text-neutral-200 hover:text-primary-400"
-                    )}
-                    href={path}
-                  >
-                     {title === "Login" && user ? "Dashboard" : title}
-                    {pathname == path && (
-                      <div className="absolute w-5 h-[3px] -translate-x-1/2 rounded-custom -bottom-2 left-1/2 bg-primary-400"></div>
-                    )}
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+                <div className="container">
+                    <div className="w-full py-4 mx-auto lg:flex md:justify-between md:items-center">
+                        {/* Logo and Hamburger */}
+                        <div className="flex items-center justify-between w-full">
+                            <Link
+                                href="/"
+                                className="flex items-center justify-center gap-2 text-2xl font-semibold text-neutral-100"
+                            >
+                                <LogoSVG width={50} height={50} bg={false} />
+                                <span className="flex">
+                                    <span className="text-primary-500 font-brush">Ampe</span>
+                                    <span className="text-white font-brush">RASA</span>
+                                </span>
+                            </Link>
+                            <Hamburger isOpen={isMenuOpen} setIsOpen={handleMenuToggle} />
+                        </div>
+
+                        {/* Navigation Links */}
+                        <div
+                            className={clsx(
+                                isMenuOpen ? "opacity-100 visible" : "opacity-0 invisible",
+                                "lg:visible transition absolute inset-x-0 z-40 w-full px-6 py-4 duration-300 ease-in-out bg-neutral-900 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center"
+                            )}
+                        >
+                            <div className="flex flex-col items-center gap-8 lg:flex-row lg:mx-6">
+                                {links.map(({ id, title, path }) => (
+                                    <Link
+                                        key={id}
+                                        onClick={pathname === path || (pathname === "/dashboard" && path === "/login") ? handleMenuItemClick : handleMenuToggle}
+                                        className={clsx(
+                                            "relative my-2 transition-colors duration-300 transform md:mx-4 md:my-0",
+                                            pathname === path || (pathname === "/dashboard" && path === "/login")
+                                                ? "text-primary-400"
+                                                : "text-neutral-200 hover:text-primary-400"
+                                        )}
+                                        href={path}
+                                    >
+                                        {title === "Login" && user ? "Dashboard" : title}
+                                        {(pathname == path || (pathname === "/dashboard" && path === "/login")) && (
+                                            <div className="absolute w-5 h-[3px] -translate-x-1/2 rounded-custom -bottom-2 left-1/2 bg-primary-400"></div>
+                                        )}
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 }
