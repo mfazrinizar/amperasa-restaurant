d0545133df76f205afd360d2a549ccb74995c65e
M. Fazri Nizar <anon6372098.id@gmail.com>
2024-12-01 08:29:28 +0700

    Feat: add Testimonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/layout/Hero";
 import MainWrapper from "@/components/layout/MainWrapper";
 import Showcase from "@/components/layout/Showcase";
+import Testimonial from "@/components/layout/Testimonial";
 
 export default function Home() {
   return (
     <MainWrapper className="overflow-x-hidden">
       <Hero />
       <Showcase />
+      <Testimonial />
     </MainWrapper>
   );
 }
